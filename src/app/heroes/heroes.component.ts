3ae5b63cd07eab14a0741b38d8b26d13dcdac9d7
Kyra Crawford Calvert <kyracalvert@gmail.com>
2019-01-18 13:27:11 -0600

    Adds *ngFor to hide li details until selected.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'; // imports a variable from the component
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
   heroes = HEROES;
-  // ^ define a component property to expose
-  // HEROES array for binding
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+}
 }
