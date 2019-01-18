91e040f3bd4f553b03fba2411129b62263fd7b8b
Kyra Crawford Calvert <kyracalvert@gmail.com>
2019-01-18 12:37:31 -0600

    creates mock-heroes file  and HEROES variables, creating a HERO object.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Franklin' },
+    { id: 12, name: 'Orlando' },
+    { id: 13, name: 'Mud' },
+    { id: 14, name: 'Puck' },
+    { id: 15, name: 'Stanley' },
+    { id: 16, name: 'Wilfredo' },
+    { id: 17, name: 'Makinde' },
+    { id: 18, name: 'Delaine' },
+    { id: 19, name: 'Cheyfaun' },
+    { id: 20, name: 'Tobe' }
+];
+export class MockHeroes {
+}
