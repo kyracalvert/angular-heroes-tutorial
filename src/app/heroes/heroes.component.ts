b231a167cf871df7a6cc641d3592522a7b774f85
Kyra Crawford Calvert <kyracalvert@gmail.com>
2019-01-18 12:45:03 -0600

    Lists heroes using ngFor in an UL

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'; // imports a variable from the component
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,10 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
+  // ^ define a component property to expose
+  // HEROES array for binding
 
   constructor() { }
 
